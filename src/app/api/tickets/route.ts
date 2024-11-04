cc58a01a1ec6ca0363baa478207fe1a7e750707d
Nicolas Chirino <nicolas.brch@gmail.com>
2024-11-04 16:28:14 -0300

    Actualizar POST tickets, moviendolo a API en vez de actions.

--- src/app/api/tickets/route.ts
+++ src/app/api/tickets/route.ts
@@ -1,3 +1,4 @@
+import { z } from "zod"
 import { db } from "@/db"
 import { tickets, user, states, categories, sla } from "@/db/schema"
 import { eq } from "drizzle-orm";
@@ -21,4 +22,56 @@ export async function GET() {
     .leftJoin(categories, eq(tickets.categoryId, categories))
     .leftJoin(sla, eq(tickets.slaId, sla));
   return Response.json(ticket);
+}
+
+const createTicketSchema = z.object({
+  title: z.string(),
+  state: z.string(),
+  category: z.string(),
+  sla: z.string(),
+  userId: z.string(),
+});
+
+export async function POST(request: Request) {
+  const formData = await request.formData();
+
+  const validatedFields = createTicketSchema.safeParse({
+    title: formData.get("title"),
+    state: formData.get("state"),
+    category: formData.get("category"),
+    sla: formData.get("sla"),
+    userId: formData.get("userId"),
+  });
+
+  if (!validatedFields.success){
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+    }
+  }
+
+  const categoryIdResult = await db.select({
+    id: categories.id,
+  }).from(categories).where(eq(categories.name, validatedFields.data.category));
+  const stateIdResult = await db.select({
+    id: states.id,
+  }).from(states).where(eq(states.name, validatedFields.data.state));
+  const slaIdResult = await db.select({
+    id: sla.id,
+  }).from(sla).where(eq(sla.name, validatedFields.data.sla));
+
+  const categoryId = categoryIdResult[0]?.id;
+  const stateId = stateIdResult[0]?.id;
+  const slaId = slaIdResult[0]?.id;
+
+  await db
+    .insert(tickets)
+    .values({
+      title: validatedFields.data.title,
+      userId: validatedFields.data.userId,
+      categoryId: categoryId,
+      stateId: stateId,
+      slaId: slaId,
+    })
+
+  return Response.json({ message: "Ticket created successfully" });
 }
