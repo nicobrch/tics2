1b7fe97b34e7ebbc7c37b4cb0cf667e98ac4d295
Nicolas Chirino <nicolas.brch@gmail.com>
2024-11-04 15:36:45 -0300

    Actualizar GET ALL tickets.

--- src/app/api/tickets/route.ts
+++ src/app/api/tickets/route.ts
@@ -1,17 +1,24 @@
 import { db } from "@/db"
-import { tickets, user } from "@/db/schema"
+import { tickets, user, states, categories, sla } from "@/db/schema"
 import { eq } from "drizzle-orm";
 
 export const dynamic = 'force-dynamic'; // defaults to auto
 
 export async function GET() {
-  const ticket = await db.select({
-    id: tickets.id,
-    title: tickets.title,
-    status: tickets.status,
-    priority: tickets.priority,
-    assignee: user.name,
-    created: tickets.createdAt
-  }).from(tickets).leftJoin(user, eq(tickets.userId, user.id));
+  const ticket = await db
+    .select({
+      id: tickets.id,
+      title: tickets.title,
+      state: states.name,
+      sla: sla.name,
+      category: categories.name,
+      assignee: user.name,
+      created: tickets.createdAt
+    })
+    .from(tickets)
+    .leftJoin(user, eq(tickets.userId, user.id))
+    .leftJoin(states, eq(tickets.stateId, states.id))
+    .leftJoin(categories, eq(tickets.categoryId, categories))
+    .leftJoin(sla, eq(tickets.slaId, sla));
   return Response.json(ticket);
 }
