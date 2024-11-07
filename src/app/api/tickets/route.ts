3d503fd72b6358f4abfa3c0e9a04abaec9c2b104
Nico <nicolas.brch@gmail.com>
2024-11-07 02:02:00 -0300

    Nuevos API endpoints para obtener datos de usuarios y tickets

--- src/app/api/tickets/route.ts
+++ src/app/api/tickets/route.ts
@@ -1,87 +1,73 @@
-import { z } from "zod"
 import { db } from "@/db"
-import { tickets, user, states, categories, sla } from "@/db/schema"
+import { tickets, users, states, categories, sla } from "@/db/schema"
 import { eq } from "drizzle-orm";
+import { createTicketSchema } from '@/types/ticket';
 
-export const dynamic = 'force-dynamic'; // defaults to auto
-
-export const selectTicketsSchema = z.object({
-  id: z.string(),
-  title: z.string(),
-  state: z.string(),
-  sla: z.string(),
-  category: z.string(),
-  assignee: z.string(),
-  created: z.string(),
-});
+export const revalidate = 60;
 
 export async function GET() {
-  const ticket = await db
-    .select({
-      id: tickets.id,
-      title: tickets.title,
-      state: states.name,
-      sla: sla.name,
-      category: categories.name,
-      assignee: user.name,
-      created: tickets.createdAt
-    })
-    .from(tickets)
-    .leftJoin(user, eq(tickets.userId, user.id))
-    .leftJoin(states, eq(tickets.stateId, states.id))
-    .leftJoin(categories, eq(tickets.categoryId, categories.id))
-    .leftJoin(sla, eq(tickets.slaId, sla.id));
-  return Response.json(ticket);
+    const ticket = await db
+        .select({
+            id: tickets.id,
+            title: tickets.title,
+            state: states.name,
+            sla: sla.name,
+            category: categories.name,
+            assignee: users.name,
+            created: tickets.createdAt
+        })
+        .from(tickets)
+        .leftJoin(users, eq(tickets.userId, users.id))
+        .leftJoin(states, eq(tickets.stateId, states.id))
+        .leftJoin(categories, eq(tickets.categoryId, categories.id))
+        .leftJoin(sla, eq(tickets.slaId, sla.id));
+    return Response.json(ticket);
 }
 
-const createTicketSchema = z.object({
-  title: z.string(),
-  state: z.string(),
-  category: z.string(),
-  sla: z.string(),
-  userId: z.string(),
-});
-
 export async function POST(request: Request) {
-  const formData = await request.formData();
+    const formData = await request.formData();
 
-  const validatedFields = createTicketSchema.safeParse({
-    title: formData.get("title"),
-    state: formData.get("state"),
-    category: formData.get("category"),
-    sla: formData.get("sla"),
-    userId: formData.get("userId"),
-  });
-
-  if (!validatedFields.success){
-    return Response.json({
-      error: validatedFields.error.flatten().fieldErrors,
+    const validatedFields = createTicketSchema.safeParse({
+        title: formData.get("title"),
+        state: formData.get("state"),
+        category: formData.get("category"),
+        sla: formData.get("sla"),
+        userEmail: formData.get("userEmail"),
     });
-  }
 
-  const categoryIdResult = await db.select({
-    id: categories.id,
-  }).from(categories).where(eq(categories.name, validatedFields.data.category));
-  const stateIdResult = await db.select({
-    id: states.id,
-  }).from(states).where(eq(states.name, validatedFields.data.state));
-  const slaIdResult = await db.select({
-    id: sla.id,
-  }).from(sla).where(eq(sla.name, validatedFields.data.sla));
+    if (!validatedFields.success){
+        return Response.json({
+            error: validatedFields.error.flatten().fieldErrors,
+        });
+    }
 
-  const categoryId = categoryIdResult[0]?.id;
-  const stateId = stateIdResult[0]?.id;
-  const slaId = slaIdResult[0]?.id;
+    const categoryIdResult = await db.select({
+        id: categories.id,
+    }).from(categories).where(eq(categories.name, validatedFields.data.category));
+    const stateIdResult = await db.select({
+        id: states.id,
+    }).from(states).where(eq(states.name, validatedFields.data.state));
+    const slaIdResult = await db.select({
+        id: sla.id,
+    }).from(sla).where(eq(sla.name, validatedFields.data.sla));
+    const userIdResult = await db.select({
+        id: users.id,
+    }).from(users).where(eq(users.email, validatedFields.data.userEmail));
 
-  await db
-    .insert(tickets)
-    .values({
-      title: validatedFields.data.title,
-      userId: validatedFields.data.userId,
-      categoryId: categoryId,
-      stateId: stateId,
-      slaId: slaId,
-    })
+    const categoryId = categoryIdResult[0]?.id;
+    const stateId = stateIdResult[0]?.id;
+    const slaId = slaIdResult[0]?.id;
+    const userId = userIdResult[0]?.id;
 
-  return Response.json({ message: "Ticket created successfully" });
+    await db
+        .insert(tickets)
+        .values({
+            title: validatedFields.data.title,
+            userId: userId,
+            categoryId: categoryId,
+            stateId: stateId,
+            slaId: slaId,
+        })
+
+    return Response.json({ message: "Ticket created successfully" });
 }
