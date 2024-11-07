04f660bdcd02e4c6bb3ce67665f39c5f699f7c09
Nicolas Chirino <nicolas.brch@gmail.com>
2024-11-07 12:32:20 -0300

    Quitar redundancia de API call a User

--- src/components/navigation/navbar.tsx
+++ src/components/navigation/navbar.tsx
@@ -1,7 +1,12 @@
 import MainNav from "@/components/navigation/main-nav";
 import UserNav from "@/components/navigation/user-nav";
+import type { User } from "@/types/user";
 
-export default function Navbar() {
+type NavbarProps = {
+  user: User,
+}
+
+export default function Navbar({user} : NavbarProps) {
 
   return (
     <div className="hidden flex-col bg-background md:flex">
@@ -9,7 +14,7 @@ export default function Navbar() {
         <div className="flex h-16 items-center px-4 container mx-auto">
           <MainNav/>
           <div className="ml-auto flex items-center space-x-4">
-            <UserNav/>
+            <UserNav user={user}/>
           </div>
         </div>
       </div>
