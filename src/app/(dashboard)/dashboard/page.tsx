46eaf5a85e29c0cfc9db06aae836a1d8a559c258
Nico <nicolas.brch@gmail.com>
2024-11-07 02:01:39 -0300

    Cambiar páginas para mostrar datos utilizando nuevo Auth y session

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,25 +1,26 @@
-import { auth } from "@/lib/auth";
-import { headers } from "next/headers";
-import { redirect } from "next/navigation";
-import StatisticsCards from "@/components/dashboard/statistics-cards";
+import StatisticsCards from '@/components/tickets/statistics-cards';
+import TicketsTable from '@/components/tickets/tickets-table';
 
-export default async function Home() {
-  const session = await auth.api.getSession({
-    headers: headers() // you need to pass the headers object.
-  })
-  if (!session) {
-    return redirect("/login");
-  }
+export default async function Page() {
+    const ticketsData = await fetch("http://localhost:3000/api/tickets");
+    const tickets = await ticketsData.json();
 
-  const data = await fetch("http://localhost:3000/api/tickets");
-  const tickets = await data.json();
+    const usersData = await fetch("http://localhost:3000/api/users");
+    const users = await usersData.json();
 
-  return (
-    <div className="flex-1 p-4 md:p-6">
-      <section>
-        <h1 className="font-bold text-3xl mb-4"> Estadísticas </h1>
-        <StatisticsCards tickets={tickets}/>
-      </section>
-    </div>
-  );
+    return (
+        <div className="flex-1 p-4 md:p-6">
+            <section>
+                <h1 className="font-bold text-xl mb-2"> Estadísticas </h1>
+                <StatisticsCards tickets={tickets} />
+            </section>
+            <section>
+                <h1 className="font-bold text-xl mb-2"> Últimos tickets </h1>
+                <TicketsTable tickets={tickets} users={users}/>
+            </section>
+            <section className="mt-4">
+                <h1 className="font-bold text-xl mb-2"> Gráficos </h1>
+            </section>
+        </div>
+    )
 }
--- src/app/(dashboard)/tickets/page.tsx
+++ src/app/(dashboard)/tickets/page.tsx
@@ -1,28 +1,16 @@
-import { auth } from "@/lib/auth";
-import { headers } from "next/headers";
-import { redirect } from "next/navigation";
-import TicketsTable from "@/components/dashboard/tickets-table";
+import TicketsTable from '@/components/tickets/tickets-table';
 
-export default async function Home() {
-  const session = await auth.api.getSession({
-    headers: headers() // you need to pass the headers object.
-  })
-  if (!session) {
-    return redirect("/login");
-  }
+export default async function Page() {
+    const ticketsData = await fetch("http://localhost:3000/api/tickets");
+    const tickets = await ticketsData.json();
 
-  const ticketData = await fetch("http://localhost:3000/api/tickets");
-  const tickets = await ticketData.json();
+    const usersData = await fetch("http://localhost:3000/api/users");
+    const users = await usersData.json();
 
-  const usersData = await fetch("http://localhost:3000/api/users")
-  const users = await usersData.json();
-
-  return (
-    <div className="flex-1 p-4 md:p-6">
-      <section>
-      <h1 className="font-bold text-3xl mb-4"> Tickets </h1>
-        <TicketsTable tickets={tickets} users={users}/>
-      </section>
-    </div>
-);
+    return (
+        <section className="flex-1 p-4 md:p-6">
+            <h1 className="font-bold text-3xl mb-4"> Tickets </h1>
+            <TicketsTable tickets={tickets} users={users}/>
+        </section>
+    );
 }
